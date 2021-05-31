97840e8c43759c07a2fe3111815e560a692a162b
Carson <carsonweinand18@gmail.com>
2021-05-31 12:06:06 -0400

    static trial & error

--- server/server_index.js
+++ server/server_index.js
@@ -9,14 +9,14 @@ const cors = require("cors");
 
 const port = 2002;
 
-app.use(express.static(path.join(__dirname, "..", "public")));
+app.use(express.static(path.join(__dirname, "..", "public", "index.html")));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("*/rooms/:listingId", (req, res) => {
   console.log("in app.get rooms/listingid");
-  res.sendFile(path.join(__dirname, "..", "public"));
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
 app.get("/listings/:listingId", (req, res) => {
