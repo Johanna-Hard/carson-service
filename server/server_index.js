412d1973e0bebc260b6535ba8ed3b6824cfc72f3
Carson <carsonweinand18@gmail.com>
2021-05-31 12:26:39 -0400

    static stuff

--- server/server_index.js
+++ server/server_index.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 
 const port = 2002;
 
-app.use(express.static(path.join(__dirname, "..", "public", "index.html")));
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
