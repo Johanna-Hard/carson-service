d7e68e2136920fee219070a155425cebb90059fa
Carson <carsonweinand18@gmail.com>
2021-06-01 23:43:41 -0400

    fix changes to revert

--- server/server_index.js
+++ server/server_index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const app = express();
 const bodyParser = require("body-parser");
 const path = require("path");
 const Sequelize = require("sequelize");
@@ -7,15 +6,16 @@ const { db, Listings, Reviews, Bookings } = require("../database/db_Index.js");
 const axios = require("axios");
 const cors = require("cors");
 
+const app = express();
 const port = 2002;
 
-app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(cors());
+app.use(express.static(path.join(__dirname + "/../public")));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("*/rooms/:listingId", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+  res.sendFile(path.join(__dirname, "/../public/index.html"));
 });
 
 app.get("/listings/:listingId", (req, res) => {
