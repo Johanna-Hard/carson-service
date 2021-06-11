d09af78527d760fd96ec70124b0a8c85d41885af
Carson <carsonweinand18@gmail.com>
2021-06-10 17:55:35 -0700

    fix compression typo

--- server/server_index.js
+++ server/server_index.js
@@ -1,4 +1,4 @@
-const copmression = require("compression");
+const compression = require("compression");
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
