6a956964cfc5e2c035b4569469b2950f989c3464
Carson <carsonweinand18@gmail.com>
2021-06-10 18:36:41 -0700

    remove async await on componentdidmount, test for performance

--- public/src/index.jsx
+++ public/src/index.jsx
@@ -23,7 +23,7 @@ class App extends React.Component {
     };
   }
 
-  async componentDidMount() {
+  componentDidMount() {
     let stateData = {
       numberOfGuests: null,
       pricePerNight: null,
@@ -39,7 +39,7 @@ class App extends React.Component {
     const splitPath = window.location.pathname.split("/");
     const listingId = splitPath[splitPath.length - 1];
 
-    await $.ajax({
+    $.ajax({
       method: "GET",
       url: `http://3.139.87.173:2002/listings/${listingId}`,
       success: (data) => {
@@ -56,7 +56,7 @@ class App extends React.Component {
       },
     });
 
-    await $.ajax({
+    $.ajax({
       method: "GET",
       url: `http://3.139.87.173:2002/reviews/${listingId}`,
       success: (data) => {
@@ -69,7 +69,7 @@ class App extends React.Component {
       },
     });
 
-    await $.ajax({
+    $.ajax({
       method: "GET",
       url: `http://3.139.87.173:2002/bookings/${listingId}`,
       success: (data) => {
